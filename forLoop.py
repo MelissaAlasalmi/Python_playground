6836919d1f85f3a0e0ff40dda289e88c241bb8bf
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2021-01-05 11:42:58 +0200

    first python program

---
+++ forLoop.py
@@ -0,0 +1,7 @@
+numbers = [23, 24, 93, 23, 432, 3, 254]
+
+#takes each of the numbers in 'numbers' and multiplies them by 2
+for i in range(0, len(numbers)):
+    numbers[i] = numbers[i] * 2
+
+print(numbers)
