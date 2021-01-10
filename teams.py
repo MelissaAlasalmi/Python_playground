bd855d3f63f03889ea43b03938f7ede2e9680f38
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2021-01-10 19:06:50 +0200

    yesterday's playground

--- teams.py
+++ teams.py
@@ -1,8 +1,8 @@
-import random
+import random as ran
 
 names = ["Ben", "Melissa", "Joe", "Lizzo", "Fred", "Bob"]
 teams = ["red", "blue", "green"]
 
 #Randomly assigns everyone in 'names' a team from 'teams'
 for name in names:
-    print("Hello, " + name + "! You are on team " + random.choice(teams) + "!")
+    print("Hello, " + name + "! You are on team " + ran.choice(teams) + "!")
