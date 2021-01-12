dfa4f06e4a080b1e7e638d14acab9502cad841b5
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2021-01-12 11:02:22 +0200

    pandas

--- basics/forLoopDataStruct.py
+++ basics/forLoopDataStruct.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import pandas as pd
 
 europe = {'spain':'madrid', 
           'france':'paris', 
@@ -8,6 +9,8 @@ europe = {'spain':'madrid',
 
 bmi = np.array([[1.88, 1.88, 1.82, 1.91,  1.91,  1.85],
                   [81.6, 97.5, 95.3, 85.3, 90.7, 81.6]])
+
+cars = pd.read_csv('cars.csv')
 
 #prints all the capitals ('value') of countries ('key') in a dictionary
 for key, value in europe.items() :
@@ -22,3 +25,18 @@ for value in np.nditer(weight / (height ** 2)) :
 #prints every single 'value' in the 2D numpy array 'bmi'
 for value in np.nditer(bmi) :
     print(str(value))
+
+#prints every single 'row' in the pandas dataframe 'cars' + lables
+for lable, row in cars.iterrows() :
+    print(lable)
+    print(row)
+
+#prints only the 'lable' and the 'country' of each dataframe 'cars'
+for lable, row in cars.iterrows() :
+    print(lable + ": " + row["country"])
+
+#applys 'namlen' collumn to 'cars' then prints the 'country' and the 'namlen' of each country from each dataframe
+cars["namlen"] = cars["country"].apply(len)
+print(cars)
+for lable, row in cars.iterrows() :
+    print("the length of " + row["country"] + " is " + str(row["namlen"]))
